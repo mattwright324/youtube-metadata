7a3ee5b3fb0d81d44f0ea49b7933bc1db2ac26db
Matthew Wright <mattwright324@gmail.com>
2020-01-13 22:37:53 -0500

    cleaner html building; more snippet details

--- js/youtube-metadata.js
+++ js/youtube-metadata.js
@@ -48,21 +48,154 @@
         return parsed;
     }
 
+    /**
+     * Can't access part(s): fileDetails, processingDetails, suggestions
+     * Useless part(s): player, id
+     * Every other part below:
+     */
+    const partMap = {
+        video: {
+            snippet: {
+                title: "Snippet",
+                postProcess: function (partJson) {
+                    const partDiv = $("#snippet");
+
+                    partDiv.append("<img src='" + partJson.thumbnails.medium.url + "' class='mb-15'>");
+
+                    /*const html =
+                        "<div style='font-size: 1.1em;'>" +
+                            "<p>" + partJson.title + "</p>" +
+                            "<a href='https://www.youtube.com/channel/" + partJson.channelId + "'>" + partJson.channelTitle + "</a>" +
+                        "</div>";
+
+                    partDiv.append(html);*/
+
+                    const titleHtml =
+                        "<p class='mb-15' style='font-size: 1.25em'>" + partJson.title + "</p>";
+                    partDiv.append(titleHtml);
+
+                    const authorHtml =
+                        "<p class='mb-15'><strong>Published by</strong> " +
+                            "<a href='https://www.youtube.com/channel/" + partJson.channelId + "' target='_blank'>" +
+                                partJson.channelTitle +
+                            "</a>" +
+                        "</p>";
+                    partDiv.append(authorHtml);
+
+                    const published = new Date(partJson.publishedAt);
+                    const dateHtml =
+                        "<p class='mb-15'><strong>Published on</strong> " +
+                            "<span class='orange'>" + published.toUTCString() + "</span>" +
+                            " (" + moment(published).fromNow() + ")" +
+                        "</p>";
+                    partDiv.append(dateHtml);
+
+                    if (partJson.tags) {
+                        const tagsHtml =
+                            "<p class='mb-15'><strong>Tag(s): </strong>" +
+                                "<span class='tag'>" + partJson.tags.join(" </span><span class='tag'>") + "</span>" +
+                            "</p>";
+                        partDiv.append(tagsHtml);
+                    }
+                }
+            },
+            statistics: {
+                title: "Statistics",
+                postProcess: function (partJson) {
+                    const partDiv = $("#statistics");
+                }
+            },
+            recordingDetails: {
+                title: "Geolocation",
+                postProcess: function (partJson) {
+                    const partDiv = $("#recordingDetails");
+
+                    const location = partJson.location;
+                    if (location && location.latitude && location.longitude) {
+                        const latlng = location.latitude + "," + location.longitude;
+                        const staticMap = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlng + "&zoom=13&size=1000x300&key=AIzaSyCGWanOEMEgdHqsxNDaa_ZXTZ6hoYQrnAI&markers=color:red|" + latlng;
+
+                        const html =
+                            "<a href='https://maps.google.com/maps?q=loc:"+latlng+"' target='_blank'>" +
+                                "<img src='"+ staticMap +"'>" +
+                                "<p>Click to open in Google Maps</p>" +
+                            "</a>";
+
+                        partDiv.append(html);
+                    }
+                }
+            },
+            status: {
+                title: "Status",
+                postProcess: function (partJson) {
+                    const partDiv = $("#status");
+                }
+            },
+            liveStreamingDetails: {
+                title: "Livestream Details",
+                postProcess: function (partJson) {
+                    const partDiv = $("#liveStreamingDetails");
+                }
+            },
+            localizations: {
+                title: "Localizations",
+                postProcess: function (partJson) {
+                    const partDiv = $("#localizations");
+                }
+            },
+            contentDetails: {
+                title: "Content Details",
+                postProcess: function (partJson) {
+                    const partDiv = $("#contentDetails");
+                }
+            },
+            topicDetails: {
+                title: "Topic Details",
+                postProcess: function (partJson) {
+                    const partDiv = $("#topicDetails");
+                }
+            }
+        }
+    };
+
     async function submit(parsedInput) {
         // console.log(parsedInput);
         if (parsedInput.type === 'unknown') {
             console.log("didn't recognize your input");
         } else if (parsedInput.type === 'video_id') {
             console.log('grabbing video');
+
             youtube.ajax('videos', {
-                part: 'contentDetails,id,liveStreamingDetails,localizations,player,recordingDetails,snippet,statistics,status,topicDetails',
+                part: Object.keys(partMap.video).join(','),
                 id: parsedInput.value
             }).done(function (res) {
                 console.log(res);
                 if (res.items.length) {
                     const item = res.items[0];
 
-                    $("#videoSnippet").text(JSON.stringify(item.snippet, null, 4));
+                    for (let part in partMap.video) {
+                        const section = $("#" + part);
+                        const sectionHeader = $(section.find(".section-header"));
+
+                        if (item.hasOwnProperty(part)) {
+                            sectionHeader.removeClass("unknown").addClass("good");
+                            sectionHeader.find("i").removeClass("question").addClass("check");
+
+                            section.append("<pre><code class=\"prettyprint json-lang\"></code></pre>");
+
+                            const json = section.find("code");
+                            json.text(JSON.stringify(item[part], null, 4));
+                            hljs.highlightBlock(json[0]);
+
+                            partMap.video[part].postProcess(item[part]);
+                        } else {
+                            sectionHeader.removeClass("unknown").addClass("bad");
+                            sectionHeader.find("i").removeClass("question").addClass("minus");
+
+                            section.append("<p class='mb-15 bad'>The video does not have " + part + ".</p>");
+                        }
+                    }
+                    /*$("#videoSnippet").text(JSON.stringify(item.snippet, null, 4));
                     hljs.highlightBlock(document.getElementById("videoSnippet"));
                     const published = new Date(item.snippet.publishedAt);
                     $("#videoSnippet").parent().after("<ul><li>"+item.snippet.title+"</li><li>Published by <a href='https://www.youtube.com/channel/"+item.snippet.channelId+"'>"+item.snippet.channelTitle+"</a></li><li>Published on "+published.toUTCString()+" ("+moment(published).fromNow()+")</li></ul>");
@@ -93,7 +226,7 @@
                     submit({
                         type: 'channel_id',
                         value: item.snippet.channelId
-                    });
+                    });*/
                 } else {
                     console.log('bad video_id');
                 }
@@ -162,6 +295,27 @@
             controls.inputValue = $("#value");
             controls.btnSubmit = $("#submit");
 
+            elements.videoSection = $("#video-section");
+
+            internal.buildPage(true);
+        },
+        buildPage: function(doSetup) {
+            $(".part-section").remove();
+
+            for (let part in partMap.video) {
+                const partData = partMap.video[part];
+                const html =
+                    "<div id='" + part + "' class='part-section'>" +
+                        "<div class='section-header unknown'><i class='question circle icon'></i><span>" + partData.title + "</span></div>" +
+                    "</div>";
+                elements.videoSection.append(html);
+            }
+
+            if (doSetup) {
+                internal.setupControls();
+            }
+        },
+        setupControls: function() {
             controls.inputValue.on('keypress', function (e) {
                 if (e.originalEvent.code === "Enter") {
                     controls.btnSubmit.click();
@@ -170,6 +324,7 @@
             controls.btnSubmit.on('click', function () {
                 const parsed = determineInput(controls.inputValue.val());
 
+                internal.buildPage(false);
                 submit(parsed);
             });
         }
